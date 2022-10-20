047fdeac35b6ab555e7179fe281076dead8b5491
DAIN KIM <dnkim419@gmail.com>
2022-10-20 16:17:50 +0900

    refactor: separated code
    into four files
    atoms.tsx
    components/createToDo.tsx
    components/ToDo.tsx
    components/ToDoList.tsx

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,14 @@
-function ToDo() {
-  return null;
+import { IToDo } from "../atoms";
+
+function ToDo({ text }: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>Doing</button>
+      <button>To Do</button>
+      <button>Done</button>
+    </li>
+  );
 }
 
 export default ToDo;
